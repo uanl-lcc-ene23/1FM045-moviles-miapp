f1dbb099f80492abf0bdb8f25ac8925144e2d188
Luz De Leon <luzaracelydls@gmail.com>
2023-04-23 16:16:00 -0600

    Crea modelos para Cancion y Artista

---
+++ miApp/src/app/cancion.model.ts
@@ -0,0 +1,6 @@
+export interface Cancion{
+    id: string;
+    nombre: string;
+    artista: string;
+    album: string;
+}
---
+++ miApp/src/artista.model.ts
@@ -0,0 +1,5 @@
+export interface Artista {
+    id: string;
+    nombre: string;
+    genero: string;
+}
